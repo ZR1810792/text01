0068a6d8639c029c6d01a29b57aaf351f9405dad
ZR <eebbrr5588@163.com>
2020-12-18 20:43:53 +0800

    chongtu02

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -3,6 +3,8 @@ package com.itheima.pojo;
 public class User {
 
     private String name;//年龄
+    private String eee;//年龄
+    private String aaa;//年龄
     private String sex;//性别
     private int age;
 }
